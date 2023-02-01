6bec563f119dafd5c3109e80189258532ac82cbe
minkyoung kim <minry20415@naver.com>
2023-02-01 23:15:03 +0900

    - 다음에 올 숫자

---
+++ src/main/java/algo1/Solution2.java
@@ -0,0 +1,22 @@
+package algo1;
+
+// 다음에 올 숫자
+public class Solution2 {
+	public int solution(int[] common) {
+		int answer = 0;
+
+		if(common[2]-common[1] == common[1]-common[0]) // 등차 수열 (+1)
+			return common[common.length-1] + common[2] - common[1]; // = 5
+		else
+			return common[common.length-1] * (common[2]/common[1]);
+
+	}
+
+	public static void main(String[] args) {
+		Solution2 solution2 = new Solution2();
+		int[] common = new int[]{3,6,9};
+		System.out.println(solution2.solution(common));
+	}
+}
+
+
