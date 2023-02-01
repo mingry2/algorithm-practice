671f9b21bb3358ba5cab3cb53b2d1d4d5d94a928
minkyoung kim <minry20415@naver.com>
2023-02-01 23:14:53 +0900

    - 옹알이(1)

---
+++ src/main/java/algo1/Solution1.java
@@ -0,0 +1,33 @@
+package algo1;
+
+// 옹알이(1)
+public class Solution1 {
+	public int solution(String[] babbling) {
+		int answer = 0;
+
+		String[] speakingList = {"aya", "ye", "woo", "ma"};
+
+		for (String str : babbling) {
+			String tempStr = str;
+			System.out.println("str = " + str);
+			for (String speak : speakingList) {
+				tempStr = tempStr.replace(speak, "");
+				System.out.println("speak = " + speak + ", tempStr = " + tempStr);
+			}
+			if (tempStr.length() == 0) {
+				answer++;
+			}
+		}
+		return answer;
+	}
+}
+
+class Main {
+	public static void main(String[] args) {
+		Solution1 solution = new Solution1();
+
+		String[] babbling = {"aya", "yee", "u", "maa", "wyeoo"};
+		System.out.println(solution.solution(babbling));
+	}
+
+}
